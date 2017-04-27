6bc6c1abbec562c53cda4917c21b29ab57b182b0
chenjl <18713506989@163.com>
2017-04-28 00:01:30 +0800

    1）底部导航栏完成

--- app/src/main/java/com/chenjl/klcsj/view/MainActivity.java
+++ app/src/main/java/com/chenjl/klcsj/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.chenjl.klcsj.klcsjandroid;
+package com.chenjl.klcsj.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.chenjl.klcsj.klcsjandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/chenjl/klcsj/view/fragment/HomeFragment.java
@@ -0,0 +1,10 @@
+package com.chenjl.klcsj.view.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by Administrator on 2017/4/27.
+ */
+
+public class HomeFragment extends Fragment{
+}
